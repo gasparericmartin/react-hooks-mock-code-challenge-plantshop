adb10cd15ea6401fa4fb23bedf76e05f06070314
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-01-12 15:22:45 -0500

    Set plant list state variables and initial fetch

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantList, setPlantList] = useState([])
+  const [plantDisplayList, setPlantDisplayList] = useState([])
+    
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+  }, [])  
+
+  
   return (
     <main>
       <NewPlantForm />
